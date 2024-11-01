208b1342491a6a84613faeb70a4ec3e758915ad9
MarcoRizz <65336645+MarcoRizz@users.noreply.github.com>
2024-11-01 17:05:42 +0100

    button mouse pass

--- scripts/wordpanel_parola.gd
+++ scripts/wordpanel_parola.gd
@@ -3,15 +3,11 @@ extends Button
 signal show_path(word: String)
 
 # Variabili per il click delle parole
-const max_holding_time = 1.0 #secondi
+const max_holding_time = 0.5 #secondi
 var valid_click: bool
 var click_global_position: Vector2
 var max_mouse_move = 10.0 #se sto trascinando il Panel evito che continui a cliccare la parola
 var holding_click_delta_time
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	mouse_filter = Control.MOUSE_FILTER_PASS
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
