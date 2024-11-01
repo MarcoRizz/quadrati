60bf95b25b13eb4616711b361f21cb2a9c1cc3e5
MarcoRizz <65336645+MarcoRizz@users.noreply.github.com>
2024-11-01 17:11:13 +0100

    valid_click non serve più
    Co-Authored-By: Ettore Passaro <ettore3091@gmail.com>

--- scripts/wordpanel_parola.gd
+++ scripts/wordpanel_parola.gd
@@ -4,39 +4,23 @@ signal show_path(word: String)
 
 # Variabili per il click delle parole
 const max_holding_time = 0.5 #secondi
-var valid_click: bool
-var click_global_position: Vector2
-var max_mouse_move = 10.0 #se sto trascinando il Panel evito che continui a cliccare la parola
 var holding_click_delta_time
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if is_pressed() and valid_click:
-		holding_click_delta_time += delta
-		
-		if click_global_position.distance_to(get_global_mouse_position()) > max_mouse_move:
-			valid_click = false
-			return
-		else:
-			# Se il tempo di hold è stato superato, esegui un'azione
-			if holding_click_delta_time > max_holding_time:
-				# Recupera il testo della label per comporre l'URL
-				var url = "https://www.google.com/search?q=" + text + "+vocabolario"
-				valid_click = false
-				
-				# Apre l'URL nel browser
-				OS.shell_open(url)
+	if is_pressed():
+		# Se il tempo di hold è stato superato, esegui un'azione
+		if holding_click_delta_time > max_holding_time:
+			# Recupera il testo della label per comporre l'URL
+			var url = "https://www.google.com/search?q=" + text + "+vocabolario"
+			
+			# Apre l'URL nel browser
+			OS.shell_open(url)
 
 
 func _on_button_down() -> void:
-	click_global_position = get_global_mouse_position()
 	holding_click_delta_time = 0
-	valid_click = true
-
-
-func _on_button_up() -> void:
-	valid_click = false
 
 
 func set_revealed_word() -> void:
@@ -47,6 +31,4 @@ func set_revealed_word() -> void:
 
 
 func _on_pressed() -> void:
-	if valid_click:
-		show_path.emit(text)
-		valid_click = false
+	show_path.emit(text)
